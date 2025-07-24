69a905d53cabf8636fefc3c9451669507b6e5349
Raul <raul.berrios@outlook.com>
2025-07-24 07:34:47 -0600

    20250724-0733: Register models
    Register the new models with the Django admin

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Book, Checkout
+# Models registration for the admin interface
+admin.site.register(User, UserAdmin)
+admin.site.register(Book)
+admin.site.register(Checkout)
 
-# Register your models here.
